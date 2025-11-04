73f2191f7f84cfe12160ea978c47fd299ce90b5b
yekta <ngyekta@gmail.com>
2025-11-04 22:17:55 +0300

    Fix flash animation

--- src/components/stat.tsx
+++ src/components/stat.tsx
@@ -1,6 +1,7 @@
 import { appLocale } from "@/lib/constants";
 import useFlashOnChange from "@/lib/hooks/use-flash-on-change";
 import { cn } from "@/lib/utils";
+import { useEffect, useRef, useState } from "react";
 
 export default function Stat({
   value,
@@ -69,14 +70,32 @@ function MainStat({
   isPlaceholder?: boolean;
 }) {
   const shouldFlash = useFlashOnChange(value, { enabled: !isPlaceholder });
+  const [canFlash, setCanFlash] = useState(false);
+  const canFlashTimeoutRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    if (!isPlaceholder) {
+      canFlashTimeoutRef.current = window.setTimeout(() => {
+        setCanFlash(true);
+        canFlashTimeoutRef.current = null;
+      }, 500);
+    }
+    return () => {
+      if (canFlashTimeoutRef.current !== null) {
+        clearTimeout(canFlashTimeoutRef.current);
+        canFlashTimeoutRef.current = null;
+      }
+    };
+  }, [isPlaceholder]);
 
   return (
     <div
+      data-can-flash={canFlash ? true : undefined}
       data-flash={shouldFlash ? true : undefined}
       className="flex shrink min-w-0 overflow-hidden items-center gap-0.75 font-semibold py-px group/main"
     >
-      <Icon className="group-data-flash/main:text-success-highlight text-foreground transition-colors duration-400 size-3 shrink-0 group-data-placeholder:rounded group-data-placeholder:animate-pulse group-data-placeholder:bg-muted-foreground group-data-placeholder:text-transparent" />
-      <p className="group-data-flash/main:text-success-highlight text-foreground transition-colors duration-400 shrink min-w-0 overflow-hidden leading-tight overflow-ellipsis group-data-placeholder:rounded group-data-placeholder:animate-pulse group-data-placeholder:bg-muted-foreground group-data-placeholder:text-transparent">
+      <Icon className="group-data-flash/main:text-success-highlight text-foreground group-data-can-flash/main:transition-colors group-data-can-flash/main:duration-400 size-3 shrink-0 group-data-placeholder:rounded group-data-placeholder:animate-pulse group-data-placeholder:bg-muted-foreground group-data-placeholder:text-transparent" />
+      <p className="group-data-flash/main:text-success-highlight text-foreground group-data-can-flash/main:transition-colors group-data-can-flash/main:duration-400 shrink min-w-0 overflow-hidden leading-tight overflow-ellipsis group-data-placeholder:rounded group-data-placeholder:animate-pulse group-data-placeholder:bg-muted-foreground group-data-placeholder:text-transparent">
         {value.toLocaleString(appLocale)}
       </p>
     </div>
