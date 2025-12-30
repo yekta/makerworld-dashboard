a83017df07355364213f0b27caa5a8ead916d7d2
yekta <ngyekta@gmail.com>
2025-12-30 22:43:59 +0300

    User summary section tweaks

--- src/components/home/user-summary-section.tsx
+++ src/components/home/user-summary-section.tsx
@@ -103,9 +103,11 @@ function Section({
           </p>
         </div>
       </div>
+      {/* Divider */}
       <div className="w-full md:w-px py-2 md:py-px flex items-center justify-center md:self-stretch">
-        <div className="w-1/2 md:w-full bg-border h-px md:h-full rounded-full" />
+        <div className="w-1/2 md:w-full bg-muted-more-foreground/50 h-px md:h-full rounded-full" />
       </div>
+      {/* Right Column / Bottom Row */}
       <div className="w-full flex flex-col items-center gap-0.5 md:w-1/2 md:items-start">
         <div className="w-full flex items-center justify-center md:justify-start">
           <p
